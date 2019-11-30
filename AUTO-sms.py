3ef2f4072aed2dc2844a5a2d443996839d6597d5
RajaShekar <yadavallirajashekar@gmail.com>
2019-11-30 11:46:45 +0530

    Python3 Every where so updated

--- AUTO-sms.py
+++ AUTO-sms.py
@@ -1,52 +1,26 @@
-import urllib2
-import cookielib
-from getpass import getpass
-import sys
-import os
-from stat import *
-from urllib import urlencode
-def sms(message):
-    #message = """raw_input("Enter text: ")"""
+import requests
+import json
+import datetime
+URL = 'https://www.way2sms.com/api/v1/sendCampaign'
 
-    number = "8686563675"
-    message = urlencode({'message':message})
-    message = message[message.find("=")+1:]
-    if __name__ == "__main__":    
-        username = "8686563675"
-        passwd = "T9282C"
+# get request
+def sendPostRequest(reqUrl, apiKey, secretKey, useType, phoneNo, senderId, textMessage):
+  req_params = {
+  'apikey':apiKey,
+  'secret':secretKey,
+  'usetype':useType,
+  'phone': phoneNo,
+  'message':textMessage,
+  'senderid':senderId
+  }
+  return requests.post(reqUrl, req_params)
 
-        message = "+".join(message.split(' '))
-
-     #logging into the sms site
-        url ='http://site24.way2sms.com/Login1.action?'
-        data = 'username='+username+'&password='+passwd+'&Submit=Sign+in'
-
-     #For cookies
-
-        cj= cookielib.CookieJar()
-        opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-
-     #Adding header details
-        opener.addheaders=[('User-Agent','Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120')]
-        try:
-            usock =opener.open(url, data)
-        except IOError:
-            print "error"
-            #return()
-
-        jession_id =str(cj).split('~')[1].split(' ')[0]
-        send_sms_url = 'http://site24.way2sms.com/smstoss.action?'
-        send_sms_data = 'ssaction=ss&Token='+jession_id+'&mobile='+number+'&message='+message+'&msgLen=136'
-        opener.addheaders=[('Referer', 'http://site25.way2sms.com/sendSMS?Token='+jession_id)]
-        try:
-            sms_sent_page = opener.open(send_sms_url,send_sms_data)
-        except IOError:
-            print "error"
-            #return()
-
-        print "success" 
-k='hello'
-type(k)
-k=str(hello)
-type(k)
-#sms(str(hai bro))
+# get response
+response = sendPostRequest(URL, '06S00GGWBJK6OQTAUEH7TMXCNU0B50TK', 'U44RMESW4B44NBIW', 'stage', '8686563675', 'active-sender-id', 'Training Halted at {}'.format(datetime.datetime.now()) )
+"""
+  Note:-
+    you must provide apikey, secretkey, usetype, mobile, senderid and message values
+    and then requst to api
+"""
+# print response if you want
+print (response.text)
